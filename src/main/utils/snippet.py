dcf1544231cdc4b301993358badd8d6d622e5380
ihayet <ishrakhayet@gmail.com>
2023-10-06 14:12:09 -0400

    Added functions for cleaning code snippets and running temporary versions of the code snippet
    Cleaning involves removing lines that only include comments and removing empty lines. Running temporary versions of the code snippet is necessary because we might have to run the original and different versions of the modified code multiple times.

--- src/main/utils/snippet.py
+++ src/main/utils/snippet.py
@@ -1,4 +1,6 @@
 import sys
+import os
+import re
 
 from typing import List
 from pathlib import Path
@@ -7,23 +9,59 @@ from subprocess import Popen, PIPE
 class Snippet:
     def __init__(self, snippet_path: str) -> None:
         self.snippet_path   : str       = snippet_path
-        self.code           : List[str] = []
-        
-        self.code.append(self.__read_snippet())
+        self.tmp_path       : str       = self.__create_tmp_path()
+        self.code           : List[str] = [ self.__clean_snippet(self.__read_snippet()) ]
 
     def __read_snippet(self) -> str:
         if not Path(self.snippet_path).exists(): raise FileNotFoundError('Snippet path does not exist\nSnippet path: {}\n'.format(self.snippet_path))
         snippet_file = open(self.snippet_path, 'r')
         return snippet_file.read()
 
+    def __clean_snippet(self, code: str) -> str:
+        lines = code.split('\n')
+        cleaned_lines = []
+
+        for line in lines:
+            line = re.sub(r'(?m)^ *#.*\n?', '', line) # removing lines that only have comments
+            if len(line) > 0 and not line.isspace():
+                cleaned_lines.append(line)
+        
+        return '\n'.join(cleaned_lines)
+
+    def __create_tmp_path(self) -> str:
+        tmp_path = 'data/tmp/' + self.snippet_path.split('/')[-1]
+        tmp_file = Path(tmp_path)
+        tmp_file.parent.mkdir(parents=True, exist_ok=True)
+
+        return tmp_path
+
+    def __create_tmp_file(self, idx: int) -> str:
+        with open(self.tmp_path, 'w+') as tmp_file:
+            try:
+                tmp_file.write(self.code[idx])
+            except Exception as e:
+                print('__create_tmp_file Exception: ' + e)
+
+    def __delete_tmp_file(self) -> None:
+        try:
+            os.remove(self.tmp_path)
+        except Exception as e:
+            print('__delete_tmp_file Exception: ' + e)
+
     def run(self, idx: int) -> List[str]:
         try:
-            print('\nRunning: {}\n'.format(self.snippet_path))
-            proc = Popen([sys.executable, self.snippet_path], stdout=PIPE, stderr=PIPE)
+            # setup tmp_file at tmp_path before running
+            self.__create_tmp_file(idx)
+            
+            print('\nRunning: {}\n'.format(self.tmp_path))
+            proc = Popen([sys.executable, self.tmp_path], stdout=PIPE, stderr=PIPE)
             proc.wait()
             out, err = proc.communicate()
             out = out.decode('utf-8')
             err = err.decode('utf-8')
+
+            # cleanup tmp_file at tmp_path after running
+            self.__delete_tmp_file()
             
             return out, err
         except Exception as e:
