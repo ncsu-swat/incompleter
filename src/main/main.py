36ed9edf3db7a0ed6a3f361f54fadf8c2a565295
ihayet <ishrakhayet@gmail.com>
2023-10-12 10:52:43 -0400

    Added outer loop to loop through all the snippet files in a specific directory

--- src/main/main.py
+++ src/main/main.py
@@ -2,18 +2,25 @@ from path_config import PROJECT_DIR, DATA_DIR
 from main.utils.snippet import Snippet
 from main.errors.error import Error
 
+import os
+
 if __name__ == '__main__':
-    path='{}/LExecutorFails/snippet_48.py.orig'.format(DATA_DIR)
-    s = Snippet(path)
-    out, err = s.run(0)
+    path='{}/LExecutorFails'.format(DATA_DIR)
+    
+    for file_name in os.listdir(path):
+        if file_name.endswith('.orig'):
+            file_path = os.path.join(path, file_name)
+    
+            s = Snippet(file_path)
+            out, err = s.run(0)
 
-    print('Error:\n{}\n'.format(err))
+            # print('Error:\n{}\n'.format(err))
 
-    e = Error(path, err)
-    var_name, Action_Class = e.find_action_class()
+            e = Error(file_path, err)
+            var_name, Action_Class = e.find_action_class()
 
-    action = Action_Class(snippet=s, lineno=e.lineno, var_name=var_name, var_val=1)
-    action.apply_pattern()
+            action = Action_Class(snippet=s, lineno=e.lineno, var_name=var_name, var_val=1)
+            action.apply_pattern()
 
     
 
