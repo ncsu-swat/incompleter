05081809a791b0b329512b8bf33b1afb38d3615e
Adam Scott <amscott9@ncsu.edu>
2024-03-21 04:04:49 -0400

    updated prepare.py to handle loops

--- data/new_all/prepare.py
+++ data/new_all/prepare.py
@@ -4,8 +4,12 @@ import re
 from subprocess import Popen, PIPE
 import copy
 import json
+import sys
+from subprocess import TimeoutExpired
 
 input_file_path = 'input.txt'
+problem_solutions_dir = "./ProblemSolutionsPython"
+
 if not os.path.exists(input_file_path):
     print("Warning: 'input.txt' does not exist in the current directory.")
     exit(1)
@@ -13,15 +17,29 @@ if not os.path.exists(input_file_path):
 def has_imports(code):
     return "import " in code or "from " in code
 
-def run_script_with_input(script_path, input_file_path):
+def run_script_with_input(script_path, input_file_path, timeout=None):
     with open(input_file_path, 'rb') as input_file:
         proc = Popen(['python3', script_path], stdout=PIPE, stderr=PIPE, stdin=input_file)
-        out, err = proc.communicate()
+        try:
+            out, err = proc.communicate(timeout=timeout)
+        except TimeoutExpired:
+            proc.kill() 
+            out, err = proc.communicate()  
+            raise TimeoutExpired(proc.args, timeout) 
     return out.decode('utf-8'), err.decode('utf-8')
 
+
 def parse_output(output):
-    pattern = r"\[FROM INNER-WORLD\]\nVAR=(\w+)\nTYPE=(\w+)"
-    return re.findall(pattern, output)
+    pattern = r"\[FROM INNER-WORLD\]\nVAR=(\w+)@(\d+)\nTYPE=(\w+)"
+    matches = re.findall(pattern, output)
+    
+    # Using a set for loops that print the same var many times
+    formatted_matches = set() 
+    for var, line_no, var_type in matches:
+        formatted_matches.add((var, var_type, line_no))
+
+    return list(formatted_matches)
+
 
 class PrintRemover(ast.NodeTransformer):
     def __init__(self):
@@ -35,6 +53,11 @@ class PrintRemover(ast.NodeTransformer):
 class Visitor(ast.NodeTransformer):
     def __init__(self):
         self.tmp_var_counter = 0
+        self.assignments = []
+
+    def visit_Assign(self, node):
+        self.assignments.append((node, node.lineno if hasattr(node, 'lineno') else None))
+        return node
 
     def visit_Body(self, node):
         if 'body' in dir(node):
@@ -42,6 +65,7 @@ class Visitor(ast.NodeTransformer):
             lim = len(node.body)
             while idx < lim:
                 if isinstance(node.body[idx], ast.Assign):
+                    line_no = getattr(node.body[idx], 'lineno', '0')
                     node.body.insert(idx, ast.parse(f'tmp_var{self.tmp_var_counter} = ' + ast.unparse(ast.fix_missing_locations(node.body[idx].value))))
                     node.body.insert(idx+1, ast.parse('print("[FROM INNER-WORLD]")'))
 
@@ -51,7 +75,8 @@ class Visitor(ast.NodeTransformer):
                     elif isinstance(node.body[idx+2].targets[0], ast.Attribute):
                         ident = node.body[idx+2].targets[0].attr
 
-                    node.body.insert(idx+2, ast.parse(f'print("VAR={ident}")'))
+                    # node.body.insert(idx+2, ast.parse(f'print("VAR={ident}")'))
+                    node.body.insert(idx+2, ast.parse(f'print("VAR={ident}@{line_no}")'))
                     node.body.insert(idx+3, ast.parse(f'print("TYPE="+type(tmp_var{self.tmp_var_counter}).__name__)'))
 
                     node.body[idx+4].value = ast.Name(id=f'tmp_var{self.tmp_var_counter}', ctx=ast.Load())
@@ -64,48 +89,58 @@ class Visitor(ast.NodeTransformer):
         return node
 
 class AssignmentRemover(ast.NodeTransformer):
-    def __init__(self, var_to_remove):
+    def __init__(self, var_to_remove, line_no_to_remove):
         self.var_to_remove = var_to_remove
+        self.line_no_to_remove = int(line_no_to_remove) 
 
     def visit_Assign(self, node):
-        if isinstance(node.targets[0], ast.Name) and node.targets[0].id == self.var_to_remove:
-            return None 
-        elif isinstance(node.targets[0], ast.Attribute) and node.targets[0].attr == self.var_to_remove:
-            return None 
-        return node 
+        if hasattr(node.targets[0], 'id') and node.targets[0].id == self.var_to_remove:
+            if getattr(node, 'lineno', 0) == self.line_no_to_remove:
+                return None  # Remove the assignment
+        elif hasattr(node.targets[0], 'attr') and node.targets[0].attr == self.var_to_remove:
+            if getattr(node, 'lineno', 0) == self.line_no_to_remove:
+                return None  # Remove the attribute assignment
+        return node
 
-def process_files(directory, input_file_path):
+
+
+def process_files(directory, input_file_path, specific_file=None):
     assignment_removed_dir = 'assignment_removed'
     os.makedirs(assignment_removed_dir, exist_ok=True)
     json_data = []
+    files_to_process = [specific_file] if specific_file else os.listdir(directory)
 
-    for filename in os.listdir(directory):
+    for filename in files_to_process:
         if filename.endswith('.py'):
             snippet_path = os.path.join(directory, filename)
             with open(snippet_path, 'r') as snippet_file:
                 snippet = snippet_file.read()
 
-            # Check if the snippet contains import statements
             contains_imports = has_imports(snippet)
-
-            # Parse the AST and modify it as before
             tree = ast.parse(snippet)
             visitor = Visitor()
             modified_tree = visitor.visit_Body(tree)
             modified_code = ast.unparse(ast.fix_missing_locations(modified_tree))
+            print(modified_code)
 
             with open('tmp.py', 'w') as tmp_file:
                 tmp_file.write(modified_code)
 
-            # Run modified script and capture output
-            output, _ = run_script_with_input('tmp.py', input_file_path)
+            try:
+                output, _ = run_script_with_input('tmp.py', input_file_path, timeout=30)
+            except TimeoutExpired:
+                print(f"Execution of {filename} exceeded 30 seconds and was skipped.")
+                continue  
+
             vars_and_types = parse_output(output)
+            print(vars_and_types)
+            if len(vars_and_types) > 50:
+                print(f"Error: More than 50 assignments in {filename}. Something might be wrong.")
+                sys.exit(1)
 
             original_tree = ast.parse(snippet)
-
-            # Process each variable and type captured from the output
-            for idx, (var, var_type) in enumerate(vars_and_types):
-                remover = AssignmentRemover(var)
+            for idx, (var, var_type, line_no) in enumerate(vars_and_types):
+                remover = AssignmentRemover(var, line_no)
                 modified_tree = remover.visit(copy.deepcopy(original_tree))
                 modified_code = ast.unparse(ast.fix_missing_locations(modified_tree))
 
@@ -118,17 +153,16 @@ def process_files(directory, input_file_path):
                     "snippet_name": modified_script_name,
                     "variable": var,
                     "type": var_type,
+                    "line_no": line_no,
                     "contains_imports": contains_imports
                 })
 
-    # Save the collected data into a JSON file
     json_file_path = os.path.join(assignment_removed_dir, 'assignments_info.json')
     with open(json_file_path, 'w') as json_file:
         json.dump(json_data, json_file, indent=4)
 
-    print("Processing complete. Metadata saved.")
+    print("Processing complete.")
 
-# Usage
-problem_solutions_dir = './ProblemSolutionsPython'
-process_files(problem_solutions_dir, input_file_path)
-
+if __name__ == "__main__":
+    specific_file = sys.argv[1] if len(sys.argv) > 1 else None
+    process_files(problem_solutions_dir, input_file_path, specific_file=specific_file)
--- data/new_all/tmp.py
+++ data/new_all/tmp.py
@@ -1,66 +1,48 @@
-print('Enter the Nth value:')
-tmp_var0 = int(input())
-print('[FROM INNER-WORLD]')
-print('VAR=rangenumber')
-print('TYPE=' + type(tmp_var0).__name__)
-rangenumber = tmp_var0
-tmp_var1 = 1
-print('[FROM INNER-WORLD]')
-print('VAR=num')
-print('TYPE=' + type(tmp_var1).__name__)
-num = tmp_var1
-tmp_var2 = 0
-print('[FROM INNER-WORLD]')
-print('VAR=c')
-print('TYPE=' + type(tmp_var2).__name__)
-c = tmp_var2
-tmp_var3 = 0
-print('[FROM INNER-WORLD]')
-print('VAR=letest')
-print('TYPE=' + type(tmp_var3).__name__)
-letest = tmp_var3
-while c != rangenumber:
-    tmp_var4 = num
+import random
+
+def bogoSort(a):
+    tmp_var0 = len(a)
     print('[FROM INNER-WORLD]')
-    print('VAR=num2')
-    print('TYPE=' + type(tmp_var4).__name__)
-    num2 = tmp_var4
-    tmp_var5 = num
+    print('VAR=n@7')
+    print('TYPE=' + type(tmp_var0).__name__)
+    n = tmp_var0
+    while is_sorted(a) == False:
+        shuffle(a)
+
+def is_sorted(a):
+    tmp_var1 = len(a)
     print('[FROM INNER-WORLD]')
-    print('VAR=num1')
-    print('TYPE=' + type(tmp_var5).__name__)
-    num1 = tmp_var5
-    tmp_var6 = 0
+    print('VAR=n@14')
+    print('TYPE=' + type(tmp_var1).__name__)
+    n = tmp_var1
+    for i in range(0, n - 1):
+        if a[i] > a[i + 1]:
+            return False
+    return True
+
+def shuffle(a):
+    tmp_var2 = len(a)
     print('[FROM INNER-WORLD]')
-    print('VAR=sum')
-    print('TYPE=' + type(tmp_var6).__name__)
-    sum = tmp_var6
-    while num1 != 0:
-        tmp_var7 = num1 % 10
+    print('VAR=n@23')
+    print('TYPE=' + type(tmp_var2).__name__)
+    n = tmp_var2
+    for i in range(0, n):
+        tmp_var3 = random.randint(0, n - 1)
         print('[FROM INNER-WORLD]')
-        print('VAR=rem')
-        print('TYPE=' + type(tmp_var7).__name__)
-        rem = tmp_var7
-        tmp_var8 = num1 // 10
+        print('VAR=r@25')
+        print('TYPE=' + type(tmp_var3).__name__)
+        r = tmp_var3
+        tmp_var4 = (a[r], a[i])
         print('[FROM INNER-WORLD]')
-        print('VAR=num1')
-        print('TYPE=' + type(tmp_var8).__name__)
-        num1 = tmp_var8
-        tmp_var9 = sum + rem
-        print('[FROM INNER-WORLD]')
-        print('VAR=sum')
-        print('TYPE=' + type(tmp_var9).__name__)
-        sum = tmp_var9
-    if num2 % sum == 0:
-        c += 1
-        tmp_var10 = num
-        print('[FROM INNER-WORLD]')
-        print('VAR=letest')
-        print('TYPE=' + type(tmp_var10).__name__)
-        letest = tmp_var10
-    tmp_var11 = num + 1
-    print('[FROM INNER-WORLD]')
-    print('VAR=num')
-    print('TYPE=' + type(tmp_var11).__name__)
-    num = tmp_var11
-print(rangenumber, 'th Harshad number is ', letest)
+        print('VAR=@26')
+        print('TYPE=' + type(tmp_var4).__name__)
+        (a[i], a[r]) = tmp_var4
+tmp_var5 = [3, 2, 4, 1, 0, 5]
+print('[FROM INNER-WORLD]')
+print('VAR=a@30')
+print('TYPE=' + type(tmp_var5).__name__)
+a = tmp_var5
+bogoSort(a)
+print('Sorted array :')
+for i in range(len(a)):
+    (print('%d' % a[i]),)
